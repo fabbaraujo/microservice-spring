3a6b008290ebafb82ceae9dfd861408b6954f360
Fabrício Araújo <fabaraujo23@gmail.com>
2020-11-10 11:08:43 -0300

    Testes com e sem contexto do Spring

---
+++ social-multiplication-chapter3/src/test/java/microservices/book/multiplication/service/MultiplicationServiceImplTest.java
@@ -0,0 +1,38 @@
+package microservices.book.multiplication.service;
+
+import microservices.book.multiplication.domain.Multiplication;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+public class MultiplicationServiceImplTest {
+
+    @Mock
+    private RandomGeneratorService randomGeneratorService;
+    private MultiplicationServiceImpl multiplicationServiceImpl;
+
+    @BeforeEach
+    public void initMock() {
+        MockitoAnnotations.initMocks(this);
+        multiplicationServiceImpl = new MultiplicationServiceImpl(randomGeneratorService);
+    }
+
+    @Test
+    public void createRandomMultiplicationTest() {
+        //given
+        given(randomGeneratorService.generateRandomFactor()).willReturn(50, 30);
+
+        //when
+        Multiplication multiplication = multiplicationServiceImpl.createRandomMultiplication();
+
+        //then
+        assertThat(multiplication.getFactorA()).isEqualTo(50);
+        assertThat(multiplication.getFactorB()).isEqualTo(30);
+        assertThat(multiplication.getResult()).isEqualTo(1500);
+    }
+}
